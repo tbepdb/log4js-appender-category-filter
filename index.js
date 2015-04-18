36e7042322de7a5a0e01c9f9ce06d49d0a26cfc5
basil <basil.belokon@gmail.com>
2015-04-18 13:02:09 +0600

    fix

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 'use strict';
 /*jslint unparam: true, indent:2, nomen: true, plusplus: true, vars: true, todo: true */
-var log4js = require('log4js');
+var log4js = require('../log4js');
 
 function categoryFilter(categories, appender) {
+  if (typeof categories === 'string') {
+    categories = [categories];
+  }
   return function (logEvent) {
-    if (categories.some(function (x) {return (x === logEvent.category); })) {
+    if (categories.indexOf(logEvent.categoryName) > -1) {
       appender(logEvent);
     }
   };
@@ -13,12 +16,9 @@ function categoryFilter(categories, appender) {
 function configure(config) {
   log4js.loadAppender(config.appender.type);
   var appender = log4js.appenderMakers[config.appender.type](config.appender);
-  if (!(config.category instanceof Array)) {
-    config.category = [config.category];
-  }
-
   return categoryFilter(config.category, appender);
 }
 
 exports.appender = categoryFilter;
 exports.configure = configure;
+
